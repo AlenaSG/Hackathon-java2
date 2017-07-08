d05d52525417e4ff8e2d03d4efb3209147480173
Alena Golovina <alenasgolovina@gmail.com>
2017-07-07 17:20:02 -0700

    add templates to display list of members in a team

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,6 @@
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
-//import java.util.ArrayList;
-//import java.util.List;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
@@ -14,14 +12,16 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      //model.put("members", request.session().attribute("members"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/members/new", (request, response) -> {
+    //new route for new members, to create them within teams
+    get("teams/:id/members/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/member-form.vtl");
+      Team team = Team.find(Integer.parseInt(request.params(":id")));
+      model.put("team", team);
+      model.put("template", "templates/team-members-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -32,11 +32,15 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/members", (request,response) -> {
+    //new route for creating and adding members inside teams
+    post("/members", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      Team team = Team.find(Integer.parseInt(request.queryParams("teamId")));
       String name = request.queryParams("name");
       Member newMember = new Member(name);
-      model.put("template", "templates/success.vtl");
+      team.addMember(newMember);
+      model.put("team", team);
+      model.put("template", "templates/team-member-success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -76,6 +80,8 @@ public class App {
       model.put("template", "templates/team.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+
 
 
   }
